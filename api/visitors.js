d2497b4b5b47379345429e1da7df983710bbda05
Marawan Mokhtar <105330382+marawan10@users.noreply.github.com>
2025-10-03 12:09:15 -0700

    Update visitors.js

--- api/visitors.js
+++ api/visitors.js
@@ -1,7 +1,7 @@
 // Vercel serverless function for visitor counter
 // This will track unique visitors using localStorage and a simple API
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
